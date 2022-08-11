1be4fe84c60625997e162c59e3b2ac6fa15605f9
Daniel Felipe Moncada Tello <110641774+Firerob@users.noreply.github.com>
2022-08-10 20:42:10 -0500

    Add files via upload

---
+++ 0x05-Functions.py.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def funcionSuma(number0,number1):
+    print("suma",number0+number1)
+          
+def funcionResta(number0,number1):
+    print("resta",number0-number1)
+
+def funcionMultiplicacion(number0, number1):
+    print("multiplicacion",number0*number1)
+
+def funcionPotencia(number0, number1):
+    print("potencia",number0**number1)
+
+
+funcionSuma(3,8)
+print("------------")
+          
+funcionResta(5,11)
+print("------------")
+          
+funcionMultiplicacion(4,8)
+print("------------")
+          
+funcionPotencia(5,2)
+print("------------")       
+        
+
+
